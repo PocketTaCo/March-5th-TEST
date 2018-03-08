eab2efb7d0a217625e4cb0cc06ea9097ea95a69d
PocketTaCo <wizardrobe8@gmail.com>
2018-03-07 14:39:48 -1000

    hi

---
+++ app.js
@@ -0,0 +1,7 @@
+var M = ["glasses", "metal", "plastic", "wood"];
+var Text = "";
+var i
+for (var i = 0; i < M.length; i++) {
+		Text += M[i] + "<br>";
+	}
+document.getElementsByClassName("Article").innerHTML
